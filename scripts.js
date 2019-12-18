0f25ecd1096e645b3ee4a25af84cfd79c69bcb69
LilyCK <57350831+LilyCK@users.noreply.github.com>
2019-12-18 20:49:59 +1300

    add hamburger menu

---
+++ scripts.js
@@ -0,0 +1,10 @@
+console.log("Hello! Welcome to my portfolio site! Thank you for stopping by :)")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
